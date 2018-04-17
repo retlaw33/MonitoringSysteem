f4a74899452f565136c3c571ae574bdd662e016e
DESKTOP-G878AUQ\Walter <w.kuijlaars@tans.net>
2018-04-17 14:18:56 +0200

    #develop - update.

--- src/main/java/Service/NodeService.java
+++ src/main/java/Service/NodeService.java
@@ -1,6 +1,7 @@
 package Service;
 
 import Domain.Node;
+import org.apache.http.client.HttpClient;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -14,48 +15,47 @@ import java.util.List;
 
 public class NodeService {
     List<Node> nodes;
+    HttpClient httpClient;
 
     public NodeService() throws IOException, ParseException {
+        nodes = new ArrayList<Node>();
         loadNodesFromJson();
     }
 
     private void loadNodesFromJson() throws IOException, ParseException {
-        List<String> endPoints = new ArrayList<String>();
-
+        //Read json array
         JSONParser parser = new JSONParser();
-
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("Nodes.json").getFile());
-
         JSONArray jsonArray = (JSONArray) parser.parse(new FileReader(file));
+        System.out.println("reading: " + file.getAbsolutePath());
 
+        //Add node to list of nodes for each object in the json array
         for (Object jsonObj : jsonArray)
         {
             JSONObject jsonNode = (JSONObject) jsonObj;
 
             String name = (String) jsonNode.get("name");
-            System.out.println(name);
+            System.out.println("name = " + name);
 
             String ip = (String) jsonNode.get("ip");
-            System.out.println(ip);
+            System.out.println("ip = " + ip);
 
             String port = (String) jsonNode.get("port");
-            System.out.println(port);
+            System.out.println("port = " + port);
 
+            List<String> endPoints = new ArrayList<String>();
             JSONArray lastPartEndPoints = (JSONArray) jsonNode.get("lastPartEndPoints");
 
-            for (Object endPoint : lastPartEndPoints)
+            for (Object ep : lastPartEndPoints)
             {
-                System.out.println(endPoint);
+                String endPoint = "http://" + ip + ":" + String.valueOf(port) + "/" + ep.toString();
+                endPoints.add(endPoint);
+                System.out.println("endPoint added: " + endPoint);
             }
+
+            nodes.add(new Node(name, endPoints));
+            System.out.println("Node added: " + name);
         }
-
-        /*for (String lpep : lastPartEndPoints) {
-            String endPoint = "http://" + ip + ":" + String.valueOf(port) + "/" + lpep;
-            endPoints.add(endPoint);
-            System.out.println("endPoint added: " + endPoint);
-        }*/
-
-        //nodes.add(new Node())
     }
 }
